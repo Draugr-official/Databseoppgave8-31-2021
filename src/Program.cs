2154ba3f8ae19c14bca6b43c23c6a75e37dd7044
NativeX64 <57600814+Draugr-official@users.noreply.github.com>
2021-09-02 15:30:19 +0200

    Update and rename Program.cs to src/Program.cs

---
+++ src/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Databaseoppgave8_31_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SqlClient sqlClient = new SqlClient("localhost", "winDB", "8asd80da0das089sad098dsa98ads", "elever");
+            sqlClient.Query("SELECT * WHERE ");
+        }
+    }
+}
